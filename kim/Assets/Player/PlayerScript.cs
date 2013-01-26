10dee59c8db2ebe2f8b088165c25acf59ae5cc7e
Olivia Zhang <olivia_ny_zhang@hotmail.com>
2013-01-26 09:08:38 -0800

    added player jumping and running

--- kim/Assets/Player/PlayerScript.cs
+++ kim/Assets/Player/PlayerScript.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 	
+	public static float defaultSpeed = 5.0f;
+	public static float slowDownSpeed = 3.0f;
+	
+	private float speed = 5.0f;
+	
+	public float gravity = 9.80f;
+	public float jumpSpeed = 8.0f;
+	
+	private Vector3 moveDirection =  Vector3.zero;
 	
 	public static float distanceTraveled;
 	// Use this for initialization
@@ -11,8 +20,39 @@ public class PlayerScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.Translate(5f*Time.deltaTime, 0f, 0f);
+	void FixedUpdate () {
+		//transform.Translate(5f*Time.deltaTime, 0f, 0f);
+		Move();
 		distanceTraveled += 5f*Time.deltaTime;
+	}
+	
+	
+	void Move(){
+		
+		if (Input.GetKeyDown(KeyCode.A)){
+			speed = slowDownSpeed;
+		}
+		else if (Input.GetKeyDown(KeyCode.D)){
+			speed = defaultSpeed;
+			
+		}
+		
+		transform.position += transform.right * speed * Time.deltaTime;
+		
+		
+			CharacterController controller = GetComponent<CharacterController>();
+			if (controller.isGrounded){
+				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            	moveDirection = transform.TransformDirection(moveDirection);
+            	moveDirection *= speed;
+            	if (Input.GetButton("Jump"))
+                	moveDirection.y = jumpSpeed;
+			}
+			
+			moveDirection.y -= gravity * Time.deltaTime;
+        	controller.Move(moveDirection * Time.deltaTime);
+			
+		
+		
 	}
 }
